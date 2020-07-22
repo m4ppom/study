a428eb85bbf0ec2632666e62960f314035ae7442
marsa <m4ppom@gmail.com>
2020-07-22 21:57:22 +0900

    blazor CRUD sample | 20200722

---
+++ c_sharp/blazor/AntCommunityBlazorApp/AntCommunityBlazorApp/Server/Controllers/EmployeeController.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using AntCommunityBlazorApp.Server.DataAccess;
+using AntCommunityBlazorApp.Shared.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AntCommunityBlazorApp.Server.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        EmployeeDataAccessLayer objemployee = new EmployeeDataAccessLayer();
+
+        [HttpGet]
+        [Route("api/Employee/Index")]
+        public IEnumerable<Employee> Index()
+        {
+            return objemployee.GetAllEmployees();
+        }
+
+        [HttpPost]
+        [Route("api/Employee/Create")]
+        public void Create([FromBody] Employee employee)
+        {
+            if (ModelState.IsValid)
+                objemployee.AddEmployee(employee);
+        }
+
+        [HttpGet]
+        [Route("api/Employee/Details/{id}")]
+        public Employee Details(int id)
+        {
+
+            return objemployee.GetEmployeeData(id);
+        }
+
+        [HttpPut]
+        [Route("api/Employee/Edit")]
+        public void Edit([FromBody] Employee employee)
+        {
+            if (ModelState.IsValid)
+                objemployee.UpdateEmployee(employee);
+        }
+
+        [HttpDelete]
+        [Route("api/Employee/Delete/{id}")]
+        public void Delete(int id)
+        {
+            objemployee.DeleteEmployee(id);
+        }
+    }
+}
