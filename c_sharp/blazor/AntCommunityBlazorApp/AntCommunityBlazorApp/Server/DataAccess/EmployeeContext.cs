3776b91ed8ed0f75cfab7a0f630e1e62924fbe7d
이승진 <m4ppom@gmail.com>
2020-07-22 18:01:00 +0900

    blz

---
+++ c_sharp/blazor/AntCommunityBlazorApp/AntCommunityBlazorApp/Server/DataAccess/EmployeeContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AntCommunityBlazorApp.Server.DataAccess
+{
+    public class EmployeeContext
+    {
+        public virtual DbSet<Employee> tblEmployee { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer(@"Put Your Connection string here");
+            }
+        }
+    }
+}
---
+++ c_sharp/blazor/AntCommunityBlazorApp/AntCommunityBlazorApp/Shared/Entities/Community2.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AntCommunityBlazorApp.Shared.Entities
+{
+    public class Community2
+    {
+        public string Title { get; set; }
+        public string Name { get; set; }
+        public DateTime Releasedate { get; set; }
+    }
+}
