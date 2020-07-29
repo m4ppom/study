1f8940843517a0b44fe6f555b1846bf699feb99f
이승진 <m4ppom@gmail.com>
2020-07-29 19:24:14 +0900

    blazor crud maria db| 2020729

--- c_sharp/blazor/BlazorApp3/BlazorApp3/Models/UpdateCommunityModel.cs
+++ c_sharp/blazor/BlazorApp3/BlazorApp3/Models/UpdateCommunityModel.cs
@@ -3,20 +3,15 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.ComponentModel.DataAnnotations;
-
 namespace BlazorApp3.Models
 {
-    public class DisplayCommunityModel
+    public class UpdateCommunityModel
     {
-       
-        [StringLength(15, ErrorMessage = "Name Is Too Long.")]
         
-        public string Name { get; set; }
-    
         public string Title { get; set; }
         [StringLength(500, ErrorMessage = "Text Is Too Long.")]
         public string Text { get; set; }
-       
+
         [EmailAddress]
         public string EmailAddress { get; set; }
     }
