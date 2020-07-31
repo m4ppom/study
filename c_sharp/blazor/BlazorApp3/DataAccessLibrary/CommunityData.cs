46747f5075d5299643f8c86f513b9b333fb89117
이승진 <m4ppom@gmail.com>
2020-07-31 17:39:14 +0900

    blazor mssql -> mariaDB | 2020 07 31

--- c_sharp/blazor/BlazorApp3/DataAccessLibrary/CommunityData.cs
+++ c_sharp/blazor/BlazorApp3/DataAccessLibrary/CommunityData.cs
@@ -15,36 +15,36 @@ namespace DataAccessLibrary
         }
         public Task<List<CommunityModel>> GetCommunity()
         {
-            //string sql = "select * from CommunityTable";
-            string sql = "select * from dbo.CommunityTable order by CreatedDate DESC";
+            string sql = "select * from CommunityTable order by CreatedDate DESC";
+            //string sql = "select * from dbo.CommunityTable order by CreatedDate DESC";
             return _db.LoadData<CommunityModel, dynamic>(sql, new { });
         }
         public Task InsertCommunity(CommunityModel community)
         {
-            //string sql = @"insert into CommunityTable (Name, Title, Text, EmailAddress)
-//values (@Name, @Title, @Text, @EmailAddress);";
-            string sql = @"insert into dbo.CommunityTable (Name, Title, Text, EmailAddress)
+            string sql = @"insert into CommunityTable (Name, Title, Text, EmailAddress)
 values (@Name, @Title, @Text, @EmailAddress);";
+            //string sql = @"insert into dbo.CommunityTable (Name, Title, Text, EmailAddress)
+//values (@Name, @Title, @Text, @EmailAddress);";
             return _db.SaveData(sql, community);
         }
         public Task DeleteCommunity(CommunityModel communityDel)
         {
-            //string sql = @"delete from CommunityTable where Id = @Id ;";
-            string sql = @"delete from dbo.CommunityTable where Id = @Id ;";
+            string sql = @"delete from CommunityTable where Id = @Id ;";
+            //string sql = @"delete from dbo.CommunityTable where Id = @Id ;";
             return _db.SaveData(sql, communityDel);
         }
 
         public Task UpdateCommunity(CommunityModel communityUp)
         {
-            //string sql = @"Update CommunityTable set Name=@Name, Title = @Title, Text =@Text, EmailAddress = @EmailAddress where Id = @Id ;";
-            string sql = @"Update dbo.CommunityTable set Name=@Name, Title = @Title, Text =@Text, EmailAddress = @EmailAddress where Id = @Id ;";
+            string sql = @"Update CommunityTable set Name=@Name, Title = @Title, Text =@Text, EmailAddress = @EmailAddress where Id = @Id ;";
+            //string sql = @"Update dbo.CommunityTable set Name=@Name, Title = @Title, Text =@Text, EmailAddress = @EmailAddress where Id = @Id ;";
             return _db.SaveData(sql, communityUp);
         }
 
         public Task<CommunityModel> getCommunitybyId(int community)
         {
-            string sql = "select * from dbo.CommunityTable where Id = @community;";
-            
+            string sql = "select * from CommunityTable where Id = @community;";
+            //string sql = "select * from dbo.CommunityTable where Id = @community;";
             return _db.LoadDataOne<CommunityModel, dynamic>(sql, new { });
         }
     }
--- c_sharp/blazor/BlazorApp3/DataAccessLibrary/SqlDataAccess.cs
+++ c_sharp/blazor/BlazorApp3/DataAccessLibrary/SqlDataAccess.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.Extensions.Configuration;
+using MySql.Data.MySqlClient;
 using System;
 using System.Data;
 using System.Data.SqlClient;
@@ -22,7 +23,8 @@ namespace DataAccessLibrary
         public async Task<List<T>> LoadData<T, U>(string sql, U parameters)
         {
             string connectionString = _config.GetConnectionString(ConnectionStringName);
-            using (IDbConnection connection = new SqlConnection(connectionString))
+            //using (IDbConnection connection = new SqlConnection(connectionString))
+            using (IDbConnection connection = new MySqlConnection(connectionString))
             {
                 var data = await connection.QueryAsync<T>(sql, parameters);
                 return data.ToList();
@@ -32,7 +34,8 @@ namespace DataAccessLibrary
         public async Task<T> LoadDataOne<T, U>(string sql, U parameters)
         {
             string connectionString = _config.GetConnectionString(ConnectionStringName);
-            using (IDbConnection connection = new SqlConnection(connectionString))
+            using (IDbConnection connection = new MySqlConnection(connectionString))
+            //using (IDbConnection connection = new SqlConnection(connectionString))
             {
                 var data = await connection.QueryFirstAsync<T>(sql, parameters);
                 return data;
@@ -42,7 +45,8 @@ namespace DataAccessLibrary
         public async Task SaveData<T>(string sql, T parameters)
         {
             string connectionnString = _config.GetConnectionString(ConnectionStringName);
-            using (IDbConnection connection = new SqlConnection(connectionnString))
+            using (IDbConnection connection = new MySqlConnection(connectionnString))
+            //using (IDbConnection connection = new SqlConnection(connectionnString))
             {
                 await connection.ExecuteAsync(sql, parameters);
             }
